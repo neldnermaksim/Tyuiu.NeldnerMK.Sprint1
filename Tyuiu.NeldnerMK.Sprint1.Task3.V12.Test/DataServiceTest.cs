c60c78f3c0a324b53a6b90b4b1427362791200ed
Maksim <maksimnelder8@gmail.com>
2025-10-03 00:00:34 +0500

    Task3

---
+++ Tyuiu.NeldnerMK.Sprint1.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NeldnerMK.Sprint1.Task3.V12.Lib;
+
+namespace Tyuiu.NeldnerMK.Sprint1.Task3.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double lengthCathetus1 = 2;
+            double lengthCathetus2 = 4;
+
+            var res = ds.TriangleArea(lengthCathetus1, lengthCathetus2);
+            Assert.AreEqual(4, res);
+
+        }
+    }
+}
+
--- Tyuiu.NeldnerMK.Sprint1.Task3.V12/Program.cs
+++ Tyuiu.NeldnerMK.Sprint1.Task3.V12/Program.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.NeldnerMK.Sprint1.Task2.V22.Lib;
+using Tyuiu.NeldnerMK.Sprint1.Task3.V12.Lib;
 
 
-namespace Tyuiu.NeldnerMK.Sprint1.Task2.V22
+namespace Tyuiu.NeldnerMK.Sprint1.Task3.V12
 {
     class Program
     {
@@ -17,34 +17,29 @@ namespace Tyuiu.NeldnerMK.Sprint1.Task2.V22
             Console.Title = "Спринт №1 | Выполнил: Нельднер М. К. | СМАРТб-25-1";
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* Спринт #1                                                                *");
-            Console.WriteLine("* Тема: Арифметические операторы в C#                                      *");
-            Console.WriteLine("* Задание #2                                                               *");
-            Console.WriteLine("* Вариант #22                                                              *");
+            Console.WriteLine("* Тема: Операторы составного присваиванияФайл                                      *");
+            Console.WriteLine("* Задание #3                                                               *");
+            Console.WriteLine("* Вариант #12                                                              *");
             Console.WriteLine("* Выполнил: Нельднер Максим Константинович | СМАРТб-25-1                   *");
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                 *");
-            Console.WriteLine("* Заданы три числа. Вычислить среднее значение из них.                     *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, выполняет указанные расчёты и печатает результат на экране. *");
             Console.WriteLine("****************************************************************************");
-            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:Первое число (целое число), Второе число (целое число),  *");
-            Console.WriteLine("* Третье число (целое число)                                               *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:Написать программу для вычисления площади прямоугольного треугольника по длинам двух катетов  *");                                     
             Console.WriteLine();
 
 
-            Console.Write("Введите первое число: ");
-            int value = int.Parse(Console.ReadLine());
+            Console.Write("Введите длину первого катета: ");
+            double lengthCathetus1 = double.Parse(Console.ReadLine());
 
-            Console.Write("Введите второе число: ");
-            int valueTwo = int.Parse(Console.ReadLine());
-
-            Console.Write("Введите третье число: ");
-            int valueThree = int.Parse(Console.ReadLine());
+            Console.Write("Введите длину второго катета: ");
+            double lengthCathetus2 = double.Parse(Console.ReadLine());
 
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
             Console.WriteLine("****************************************************************************");
 
-            Console.WriteLine(ds.CalculateAVGValue(value, valueTwo, valueThree));
-
+            Console.WriteLine($"{ds.TriangleArea(lengthCathetus1, lengthCathetus2):F3}");
         }
     }
 }
